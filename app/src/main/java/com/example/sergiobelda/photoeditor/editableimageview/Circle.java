7e864717e63cc86090bdc610950342fa9799cf02
Sergio Belda <serbelga@inf.upv.es>
2019-01-02 14:34:12 +0100

    New circle class

---
+++ app/src/main/java/com/example/sergiobelda/photoeditor/editableimageview/Circle.java
@@ -0,0 +1,46 @@
+package com.example.sergiobelda.photoeditor.editableimageview;
+
+public class Circle {
+    private float x, y;
+    private double radius;
+    private float color;
+
+    public Circle(float x, float y, double radius, float color) {
+        this.x = x;
+        this.y = y;
+        this.radius = radius;
+        this.color = color;
+    }
+
+    public float getX() {
+        return x;
+    }
+
+    public void setX(float x) {
+        this.x = x;
+    }
+
+    public float getY() {
+        return y;
+    }
+
+    public void setY(float y) {
+        this.y = y;
+    }
+
+    public double getRadius() {
+        return radius;
+    }
+
+    public void setRadius(double radius) {
+        this.radius = radius;
+    }
+
+    public float getColor() {
+        return color;
+    }
+
+    public void setColor(float color) {
+        this.color = color;
+    }
+}
